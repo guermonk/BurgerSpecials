31d186f7491808ab3d4c0bd2bf9fa905226c649c
guermonk <123778338+guermonk@users.noreply.github.com>
2023-02-15 12:57:01 -0500

    Add files via upload

---
+++ frmBrgrSpecials/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(287, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(233, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Farm Burger Specials"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btn1
+        '
+        Me.btn1.AllowDrop = True
+        Me.btn1.Location = New System.Drawing.Point(80, 312)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(124, 26)
+        Me.btn1.TabIndex = 1
+        Me.btn1.Text = "Prime Beef"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Location = New System.Drawing.Point(316, 312)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(124, 26)
+        Me.btnSelect.TabIndex = 2
+        Me.btnSelect.Text = "Select Meal "
+        Me.btnSelect.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(566, 312)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(124, 26)
+        Me.btn2.TabIndex = 3
+        Me.btn2.Text = "Veggie"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(255, 354)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(288, 14)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Choose a burger, then click the Select Meal button"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(324, 378)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(114, 14)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Enjoy your burgers!"
+        '
+        'btnExit
+        '
+        Me.btnExit.Enabled = False
+        Me.btnExit.Location = New System.Drawing.Point(336, 406)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.frmBrgrSpecials.My.Resources.Resources.Veggie
+        Me.PictureBox2.Location = New System.Drawing.Point(545, 58)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(174, 248)
+        Me.PictureBox2.TabIndex = 8
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        Me.PictureBox2.WaitOnLoad = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.frmBrgrSpecials.My.Resources.Resources.OIP
+        Me.PictureBox1.Location = New System.Drawing.Point(57, 121)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(203, 179)
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        Me.PictureBox1.WaitOnLoad = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Burger Specials"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btnSelect As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ frmBrgrSpecials/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        PictureBox1.Visible = True
+        PictureBox2.Visible = False
+    End Sub
+
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        PictureBox2.Visible = True
+        PictureBox1.Visible = False
+    End Sub
+
+    Private Sub BtnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        btn1.Enabled = False
+        btn2.Enabled = False
+        btnExit.Enabled = True
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
